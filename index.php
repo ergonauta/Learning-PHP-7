676fb2cdd30ceb8a789e4f99dab05bfc21a93acf
Termigiova <termigiova@me.com>
2018-08-03 19:18:58 -0500

    Implemented Views

--- index.php
+++ index.php
@@ -3,13 +3,29 @@
     require_once __DIR__ . '/vendor/autoload.php';
 
     use Bookstore\Models\BookModel;
+    use Bookstore\Models\SaleModel;
     use Bookstore\Core\Db;
 
     $loader = new Twig_Loader_Filesystem(__DIR__ . '/views');
     $twig = new Twig_Environment($loader);
 
+    // Book View
     $bookModel = new BookModel(Db::getInstance());
-    $book = $bookModel->get(1);
+    $book = $bookModel->getAll(1,3);
 
-    $params = ['book' => $book];
+    $params = ['books' => $books, 'currentPage' => 2];
     echo $twig->loadTemplate('book.twig')->render($params);
+
+    // Sale View
+    $saleModel = new SaleModel(Db::getInstance());
+    $sales = $saleModel->getByUser(1);
+
+    $params = ['sales' => $sales];
+    echo $twig->loadTemplate('sales.twig')->render($params);
+
+    $saleModel = new SaleModel(Db::getInstance());
+    $sales = $saleModel->get(1);
+
+    $params = ['sale' => $sale];
+    echo $twig->loadTemplate('sale.twig')->render($params);
+
