cc627941073001e6ad8ee09068125a9ae54637e9
Termigiova <termigiova@me.com>
2018-08-04 03:30:28 -0500

    Deleted singleton implementation

--- src/Core/Config.php
+++ src/Core/Config.php
@@ -6,17 +6,10 @@
 
     class Config {
         private $data;
-        private static $instance;
 
         private function __construct() {
             $json = file_get_contents(__DIR__ . '/../../config/app.json');
             $this->data = json_decode($json, true);
-        }
-
-        public static function getInstance() {
-            if (self::$instance == null)
-                self::$instance = new Config();
-            return self::$instance;
         }
 
         public function get($key) {
