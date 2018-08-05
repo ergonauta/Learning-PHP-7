9b2e4ec9a871d2f78a9624d7dc29af412439796f
Termigiova <termigiova@me.com>
2018-08-05 14:31:37 -0500

    Implemented Data Providers

--- tests/Domain/Customer/CustomerFactoryTest.php
+++ tests/Domain/Customer/CustomerFactoryTest.php
@@ -34,10 +34,47 @@
             $customer = CustomerFactory::factory('deluxe', 1, 'han', 'solo', 'han@gmail.com');
         }
 
-        /**
-         * @expectedException \ InvalidArgumentException
-         */
-        public function testCreatingCorrectCustomer() {
-            $customer = CustomerFactory::factory('basic',1,'han','solo','han@gmail.com');
+//        /**
+//         * @expectedException \ InvalidArgumentException
+//         */
+//        public function testCreatingCorrectCustomer() {
+//            $customer = CustomerFactory::factory('basic',1,'han','solo','han@gmail.com');
+//        }
+
+        public function providerFactoryValidCustomerTypes() {
+            return [
+                'Basic customer, lowercase' => [
+                    'type' => 'basic',
+                    'expectedType' => '\Bookstore\Domain\Customer\Basic'
+                ],
+                'Basic customer, uppercase' => [
+                    'type' => 'BASIC',
+                    'expectedType' => '\Bookstore\Domain\Customer\Basic'
+                ],
+                'Premium customer, lowercase' => [
+                    'type' => 'premium',
+                    'expectedType' => '\Bookstore\Domain\Customer\Premium'
+                ],
+                'Premium customer, uppercase' => [
+                    'type' => 'PREMIUM',
+                    'expectedType' => '\Bookstore\Domain\Customer\Premium'
+                ]
+            ];
         }
+
+        /**
+         * @dataProvider providerFactoryValidCustomerTypes
+         * @param string $type
+         * @param string $expectedType
+         */
+        public function testFactoryValidCustomerType(string $type, string $expectedType) {
+            $customer = CustomerFactory::factory($type, 1, 'han', 'solo', 'han@gmailcom');
+
+            $this->assertInstanceOf(
+                $expectedType,
+                $customer,
+                'Factory created the wrong type of customer.'
+            );
+        }
+
     }
