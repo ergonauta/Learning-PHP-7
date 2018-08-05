4bdad98a870f1f6525bf88651b602dc7f953316b
Termigiova <termigiova@me.com>
2018-08-04 23:44:37 -0500

    Removed autoincremental function for id

--- src/Utils/Unique.php
+++ src/Utils/Unique.php
@@ -2,31 +2,12 @@
 
     namespace Bookstore\Utils;
 
-    use Bookstore\Exceptions\ExceededMaxAllowedException;
-    use Bookstore\Exceptions\InvalidIdException;
-
     trait Unique {
 
-        private static $lastID = 0;
         protected $id;
 
-        public function setID(?int $id){
-            if($id < 0)
-                throw new InvalidIdException('Id can not be negative.');
-            if (empty($id))
-                $this->id = ++self::$lastID;
-            else {
-                $this->id = $id;
-                if ($id > self::$lastID)
-                    self::$lastID = $id;
-            }
-            if ($this->id > 50) {
-                throw new ExceededMaxAllowedException('Max number of users is 50.');
-            }
-        }
-
-        public static function getLastID(): int {
-            return self::$lastID;
+        public function setID(int $id){
+            $this->id = $id;
         }
 
         public function getID(): int {
